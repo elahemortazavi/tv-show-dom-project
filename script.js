9af9bca0075954a9ee9bb1be4975d44293ae2a7d
elahemortazavi <emor5680@gmail.com>
2023-02-08 18:39:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,6 @@
 
 
 
-
-
-
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
@@ -33,24 +30,24 @@ function makeSeasonAndEpisode(episode) {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  // clear out the rootElement's HTML before we add the new stuff
+  // clear out the rootElement's HTML before we add the new stuff:
   rootElem.innerHTML = "";
   const countParagraph = document.createElement("p");
   countParagraph.innerText = `Showing ${episodeList.length} episodes`;
   rootElem.appendChild(countParagraph);
 
   episodeList.forEach((episode) => {
-    // add the season and episode and name
+    // add the season and episode and name:
     const paragraph = document.createElement("p");
     paragraph.textContent = `${makeSeasonAndEpisode(episode)}: ${episode.name}`;
     rootElem.appendChild(paragraph);
-
+  
     // add the image
     const image = document.createElement("img");
     image.src = episode.image.medium;
     rootElem.appendChild(image);
 
-    // add the summary paragraph nb the episode.summary is actually HTML
+    // add the summary paragraph nb the episode.summary is actually HTML:
     rootElem.innerHTML += episode.summary;
   });
 }
@@ -59,7 +56,7 @@ const searchInput = document.getElementById("search-input");
 searchInput.addEventListener("input", (e) => {
   const searchString = e.target.value.toLowerCase();
   const filteredEpisodes = getAllEpisodes().filter((episode) => {
-    // localeCompare might be neater here
+    // localeCompare might be neater here:
     return (
       episode.summary.toLowerCase().includes(searchString) ||
       episode.name.toLowerCase().includes(searchString)
